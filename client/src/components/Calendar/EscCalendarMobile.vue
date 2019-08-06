41337e1c0f5b59141c968669fe208a4990733262
Rodchanat K <rod8711@gmail.com>
2019-08-06 15:51:00 +0700

    add: 1st idea of mobile reservation page.

--- client/src/components/Calendar/EscCalendarMobile.vue
+++ client/src/components/Calendar/EscCalendarMobile.vue
@@ -2,9 +2,8 @@
   div.middle
     .cal-month
       p {{"<"}}
-      p.title {{Date.now()}}
+      p.title {{moment().format("D MMMM")}} - {{moment().add(6, "days").format("D MMMM")}}
       p {{">"}}
-
     .calendar
       .cal-row(v-for="idx in Array(1).fill(0)")
         .cal-col(v-for="idx in Array(7).fill(0)") {{idx}}
@@ -13,64 +12,98 @@
       b-button.action-btn(type="is-primary") Reserve by time
       b-button.action-btn(type="is-primary") Reserve by room
     hr
-    div
-      p.title avaiable
-      hr
-      .reserve-list
-        .card 
-          div.card-content
-            span.title.has-text-primary Room 1A 
-            span.header - 6 person room
-          div
-            b-button.card-action.is-primary Reserve
-            b-button.card-action.has-text-primary More detail
-        .card 
-          div.card-content
-            span.title.has-text-primary Room 2A 
-            span.header - 6 person room
-          div
-            b-button.card-action.is-primary Reserve
-            b-button.card-action.has-text-primary More detail
-        .card 
-          div.card-content
-            span.title.has-text-primary Room 1C 
-            span.header - 6 person room      
-          div
-            b-button.card-action.is-primary Reserve
-            b-button.card-action.has-text-primary More detail
-    div
+    
+    div(v-if="isReservingByTime")      
+        ReserveByTimeCard(
+          :roomName="'Room 1A'"
+          :roomType="'6 person room'"
+          :reserver="null"
+        )
+        ReserveByTimeCard(
+          :roomName="'Room 2A'"
+          :roomType="'6 person room'"
+          :reserver="null"
+        )
+        ReserveByTimeCard(
+          :roomName="'Room 1B'"
+          :roomType="'10 person room'"
+          :reserver="null"
+        )
+    div(v-if="isReservingByTime")
       p.title reserved
       hr
       .reserve-list
-        .card
-          div.card-content
-            span.title.has-text-primary Room 1B 
-            span.header - 6 person room
-            p
-              span reserved by&nbsp;
-              span.has-text-black Rodchananat K.
-          div
-            b-button.card-action.is-primary(disabled) Reserve
-            b-button.card-action.has-text-primary More detail
-        .card
-          div.card-content
-            span.title.has-text-primary Room 1B 
-            span.header - 6 person room
-            p
-              span reserved by&nbsp;
-              span.has-text-black Rodchananat K.
-          div
-            b-button.card-action.is-primary(disabled) Reserve
-            b-button.card-action.has-text-primary More detail
+        ReserveByTimeCard(
+          :roomName="'Room 2B'"
+          :roomType="'10 person room'"
+          :reserver="'Rodchananat K.'"
+        )
+        ReserveByTimeCard(
+          :roomName="'Room 1C'"
+          :roomType="'15 person room'"
+          :reserver="'Krist'"
+        )
+    div(v-if="!isReservingByTime")
+      p.title Table for Room 1A 
+      p.header Click reserve card to reserve
+      hr
+      
+      .title.is-primary =========== start - 8:00 ================
+      
+      .header.seperator มีคนจองระหว่างนี้
+      //- .card 
+      //-   div.card-content
+      //-     span.title.has-text-primary 9:30 - 12:30&nbsp;
+      //-     span.header - 3 hours
+      //-     p
+      //-       span reserved by&nbsp;
+      //-       span.has-text-black.is-size-5.has-text-weight-bold {{ 'Rodchananat' }}
+      .card.has-background-primary
+        div.card-content
+          span.title.has-text-white 12:30 - 16:00&nbsp;
+          span.header - 3:30 hours
+          p
+            span.is-size-4.has-text-weight-bold Avaiable for reserve
+          b-button.card-action.is-inverted Reserve
+      .header.seperator มีคนจองระหว่างนี้
+      //- .card 
+      //-   div.card-content
+      //-     span.title.has-text-primary 16:00 - 17:30&nbsp;
+      //-     span.header - 1:30 hours
+      //-     p
+      //-       span reserved by&nbsp;
+      //-       span.has-text-black.is-size-5.has-text-weight-bold {{ 'Krist' }}
+      //- .card 
+      //-   div.card-content
+      //-     span.title.has-text-primary 17:30 - 18:00&nbsp;
+      //-     span.header - 0:30 hours
+      //-     p
+      //-       span reserved by&nbsp;
+      //-       span.has-text-black.is-size-5.has-text-weight-bold {{ 'Someone' }}
+      .card.has-background-primary
+        div.card-content
+          span.title 18:00 - 19:00&nbsp;
+          span.header - 1 hour
+          p
+            span.is-size-4.has-text-weight-bold Avaiable for reserve
+          b-button.card-action.is-primary Reserve
 
+      .title.is-primary =========== end 19:00 ================
 </template>
 
 <script>
+import * as moment from 'moment';
+import ReserveByTimeCard from './ReserveByTimeCard';
 export default {
   data() {
     return {
-      range: [1, 2, 3, 4, 5, 6, 7]
+      range: [1, 2, 3, 4, 5, 6, 7],
+      moment, // moment lib
+      isReservingByTime: false,
     };
+  },
+  components: {
+    ReserveByTimeCard
   }
 };
 </script>
@@ -112,21 +145,15 @@ export default {
   display: flex;
   align-content: space-between;
 }
-
-.card {
-  margin-bottom: 12px;
+.seperator {
+  padding: 0px 8px;
+  background: grey;
+  margin: 8px 0;
 }
 
-.card-content {
-  padding: 8px 0 8px 16px;
-}
-.action-btn {
-  margin-left: 2.5%;
-  margin-right: 2.5%;
-  width: 45%;
-}
-
-.card-action {
-  width: 50%;
+.has-background-primary {
+  * {
+    color: white;
+  }
 }
 </style>
---
+++ client/src/components/Calendar/ReserveByTimeCard.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  .card 
+    div.card-content
+      span.title.has-text-primary {{ roomName }} &nbsp;
+      span.header - {{ roomType }}
+      p(v-if="reserver")
+        span reserved by&nbsp;
+        span.has-text-black {{ reserver }}
+    div
+      b-button.card-action.is-primary(:disabled="!!reserver") Reserve
+      b-button.card-action.has-text-primary More detail
+</template >
+
+<script>
+export default {
+    props: ["roomName", "roomType", "reserver"],
+}
+</script>
+
+<style>
+.card {
+  margin-bottom: 12px;
+}
+
+.card-content {
+  padding: 8px 0 8px 16px;
+}
+.action-btn {
+  margin-left: 2.5%;
+  margin-right: 2.5%;
+  width: 45%;
+}
+
+.card-action {
+  width: 50%;
+}
+</style>
