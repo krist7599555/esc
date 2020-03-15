13613d6cb347380971c8598635e11d2b46de637e
krist7599555 <krist7599555@gmail.com>
2020-03-16 01:09:47 +0700

    create lib sso

--- nestjs/libs/sso/src/sso.module.ts
+++ nestjs/libs/sso/src/sso.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule } from '@nestjs/common';
 import { SsoService } from './sso.service';
 
 @Module({
   providers: [SsoService],
-  exports: [SsoService],
+  imports:   [HttpModule],
+  exports:   [SsoService],
 })
 export class SsoModule {}
--- nestjs/libs/sso/src/sso.service.spec.ts
+++ nestjs/libs/sso/src/sso.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SsoService } from './sso.service';
+import { HttpModule, HttpException } from '@nestjs/common';
 
 describe('SsoService', () => {
   let service: SsoService;
+  const PASS_USERNAME = '6031301721';
+  const PASS_PASSWORD = 'krist7599555';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [SsoService],
+      imports:   [HttpModule],
     }).compile();
 
     service = module.get<SsoService>(SsoService);
@@ -14,5 +18,24 @@ describe('SsoService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('normal flow', async () => {
+    const user = await service.login(PASS_USERNAME, PASS_PASSWORD).toPromise();
+    expect(user).toBeTruthy();
+  });
+
+  function login_fail(usr, pwd) {
+    return expect(service.login(usr, pwd)).rejects.toThrowError(HttpException);
+  }
+  it('wrong username', async () => {
+    await login_fail('*', PASS_PASSWORD);
+  });
+  it('wrong password', async () => {
+    await login_fail(PASS_USERNAME, '*');
+  });
+  it('no username', async () => {
+    await login_fail(undefined, '*');
+    await login_fail(null, '*');
   });
 });
