b4f9eff89bb58b54ef11a85006baaec87298b7e9
Krist Pornpairin <krist7599555@gmail.com>
2020-05-11 17:59:14 +0700

    add tailwind

---
+++ emberjs/app/styles/tailwind.js
@@ -0,0 +1,41 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      sans:
+        '"Sukhumvit Set", -apple-system, Helvetica, Georgia, Cambria, serif',
+      serif:
+        '"Sukhumvit Set", -apple-system, Helvetica, Georgia, Cambria, serif',
+      display:
+        '"Sukhumvit Set", -apple-system, Helvetica, Georgia, Cambria, serif',
+      body:
+        '"Sukhumvit Set", -apple-system, Helvetica, Georgia, Cambria, serif',
+    },
+    colors: {
+      white: '#FFFFFF',
+      light: '#F5F5F5',
+      grey: '#DBDBDB',
+      primary: {
+        default: '#8C2E2F',
+        dark: '#682323',
+      },
+      dark: '#363636',
+    },
+    container: {
+      center: true,
+      padding: {
+        default: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+      },
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+  corePlugins: {
+    outline: false,
+  },
+}
--- emberjs/ember-cli-build.js
+++ emberjs/ember-cli-build.js
@@ -5,6 +5,20 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    postcssOptions: {
+      compile: {
+        extension: 'postcss',
+        enabled: true,
+        plugins: [
+          { module: require('postcss-import') },
+          require('tailwindcss')('./app/styles/tailwind.js'),
+        ]
+      },
+      filter: {
+        enabled: true,
+        include: ['styles/*.postcss'],
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
