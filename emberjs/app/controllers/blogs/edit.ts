3fcb3c5de3652bb86f8c42fdf136f52eaae64d56
Krist Pornpairin <krist7599555@gmail.com>
2020-05-18 00:41:15 +0700

    html header + favicon

--- emberjs/app/controllers/blogs/edit.ts
+++ emberjs/app/controllers/blogs/edit.ts
@@ -1,6 +1,7 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service, Registry as Services } from '@ember/service';
+import Blog from '../../models/blog';
 
 export default class BlogsEdit extends Controller {
   @service toast: Services["toast"]
@@ -10,8 +11,9 @@ export default class BlogsEdit extends Controller {
       .then((blog: Blog) => {
         this.transitionToRoute('blogs.show', blog);
       })
-      .catch((err) => {
+      .catch((err: any) => {
         this.toast.error("edit blogs fail");
+        console.error("BlogsEdit -> submit -> err", err)
       })
   }
 }
--- emberjs/app/controllers/blogs/new.ts
+++ emberjs/app/controllers/blogs/new.ts
@@ -1,8 +1,8 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service, Registry as Services } from '@ember/service'
 import Blog from 'emberjs/models/blog';
+
 export default class BlogsNew extends Controller {
   @service toast: Services["toast"];
   @action
