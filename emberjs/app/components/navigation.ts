9fe403a6af06dd6ed53931ca94abb7f1f466238b
Krist Pornpairin <krist7599555@gmail.com>
2020-05-18 22:55:43 +0700

    add style

--- emberjs/app/components/navigation.ts
+++ emberjs/app/components/navigation.ts
@@ -1,11 +1,25 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import { inject as service, Registry as Services } from '@ember/service';
 import { action } from '@ember/object';
 import { toLeft, toRight } from 'ember-animated/transitions/move-over';
+import { gsap } from 'gsap';
+import type Sprite from 'ember-animated/-private/sprite';
+
 
 export default class Navigation extends Component {
   @service auth: Services["auth"];
   @service toast: Services["toast"];
+  @service router: Services["router"];
+  @tracked show_menu = false;
+
+  constructor(owner: unknown, args: any) {
+    super(owner, args);
+    this.router.on("routeDidChange", () => {
+      this.show_menu = false;
+    })
+  }
+
   @action
   async logout() {
     this.auth.logout().then(() => {
@@ -18,5 +32,17 @@ export default class Navigation extends Component {
     } else {
       return toLeft;
     }
+  }
+
+  @action
+  mounted(el: HTMLElement) {
+    const is_mobile = window.innerWidth <= 767;
+    const conf = is_mobile ? { y: 100 } : { x: -100 }
+    gsap.from(el, { delay: 0.5, ...conf })
+  }
+
+  @action
+  toggle_menu() {
+    this.show_menu = !this.show_menu;
   }
 }
--- emberjs/app/styles/tailwind.js
+++ emberjs/app/styles/tailwind.js
@@ -2,6 +2,19 @@
 module.exports = {
   purge: [],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      // 'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      // 'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
     fontFamily: {
       sans:
         '"Sukhumvit Set", -apple-system, Helvetica, Georgia, Cambria, serif',
